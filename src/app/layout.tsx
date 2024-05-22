1a7a5e11110986dd000946cfb5ee889dc0a66d13
Michael Tagg <mdtagg91@gmail.com>
2024-05-21 17:22:03 -0700

    delete button added to accordian list
    Accordian extracted into client component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.css';
 import './globals.css'
+import Navbar from "./components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <header>
+          <Navbar />
+        </header>
         {children}
       </body>
     </html>
